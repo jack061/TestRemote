056da696138eb60a2d2bdc036783c8abb44037bd
lbja2 <lbja2@live.com>
2019-01-23 13:16:37 +0800

    first commit

--- TestGit/Program.cs
+++ TestGit/Program.cs
@@ -11,7 +11,7 @@ namespace TestGit
         static void Main(string[] args)
         {
             Console.WriteLine("give me enhance");
-            
+            Console.ReadKey();
         }
     }
 }
